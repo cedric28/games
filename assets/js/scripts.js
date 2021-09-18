0805b73485cdf8f8faf028b6b721da69ad950bef
zekinah lecaros <zjlecaros@gmail.com>
2021-09-18 21:41:01 +0800

    Fixes

---
+++ assets/js/scripts.js
@@ -0,0 +1,40 @@
+(function ($) {
+    'use strict';
+    $ = jQuery.noConflict();
+    var app = {
+        initMobileMenu: function() {
+            $('#mobile-menu').on('click', function(){
+                var sb = $('#sidebar');
+                if(sb.hasClass('m-active')){
+                    sb.removeClass('m-active');
+                } else {
+                    sb.addClass('m-active');
+                }
+            });
+            
+            // $('#sidebarCollapse').on('click',function(){
+            // 	$('#sidebar').toggleClass('active');
+            // });
+        },
+        initMobileResize: function() {
+            $(window).on('resize load',function(){
+                var sf = $('.search-form');
+                if ($(this).width() < 991) {
+                    sf.detach().prependTo('.mobile-search');
+                } else {
+                    sf.detach().prependTo('.desktop-search');
+                }
+            });
+        },
+        init: function() {
+            this.initMobileResize();
+            this.initMobileMenu();
+        }
+    }
+    $(document).ready(function(){
+
+        app.init();
+        
+    });  
+
+})(jQuery);
